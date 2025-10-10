d1884b1ca50c8bc65cc26b95c8c836b6ac27e397
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-10-10 11:28:05 -0400

    express config

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ dotenv.config({ path: '.env' });
 
 const app = express();
 
+app.use(express.json());
+
+// FOR DEV ONLY!!
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
