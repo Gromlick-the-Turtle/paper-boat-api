1b5417e8d7fc9a1e90f17184a9055c1cbcb8fae5
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-10-13 14:40:01 -0400

    add some basic middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import routes from '#routes/routes';
 
@@ -8,13 +10,15 @@ dotenv.config({ path: '.env' });
 const app = express();
 
 app.use(express.json());
-
-// FOR DEV ONLY!!
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.use(cookieParser());
+app.use(cors({
+    origin: [
+        'http://localhost:9000',
+        // 'paper-boats'
+    ],
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true,
+}));
 
 app.use(routes);
 
