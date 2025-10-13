8be0019e9e0093536989c58ab473d90313dd0385
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-10-13 16:04:16 -0400

    add lookups

--- src/models/lookup.js
+++ src/models/lookup.js
@@ -7,7 +7,19 @@ export default class Lookup extends ModelAbstract {
     static id = Number;
     static label = String;
 
-    static async getRoles () {
+    static async getCountries () {
+        return await db.any('SELECT * FROM l_country');
+    }
+
+    static async getStates () {
+        return await db.any('SELECT * FROM l_state');
+    }
+
+    static async getUserRoles () {
         return await db.any('SELECT * FROM l_user_role');
+    }
+
+    static async getCustomFormItemTypes () {
+        return await db.any('SELECT * FROM l_custom_form_item_type');
     }
 }
