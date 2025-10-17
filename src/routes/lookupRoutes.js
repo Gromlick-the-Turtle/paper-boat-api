4c4d3156b6c9b922fcca40f56b64909ebf741569
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-10-17 13:23:24 -0400

    add lookup routes

---
+++ src/routes/lookupRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import Lookup from '#models/Lookup';
+
+const router = express.Router();
+
+router.get('country', async (req,res) => res.json(await Lookup.getCountries()));
+
+router.get('state', async (req,res) => res.json(await Lookup.getStates()));
+
+router.get('user_role', async (req,res) => res.json(await Lookup.getUserRoles()));
+
+router.get('custom_form_item_type', async (req,res) => res.json(await Lookup.getCustomFormItemTypes()));
+
+export default router;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import _ from 'lodash';
 
+import lookupRoutes from '#routes/lookupRoutes';
 import userRoutes from '#routes/userRoutes';
 
 const routes = express.Router()
 
+routes.use('/lookup', lookupRoutes);
 routes.use('/user', userRoutes);
 
 export default routes;
