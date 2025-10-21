8bb16af9b5e226fab813627ea869ef123ff2b4c2
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-10-21 14:03:17 -0400

    first pass at simple api tests

---
+++ tests/api.spec.js
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+let ctx;
+const vars = {};
+
+test.beforeAll(async ({ playwright }) => {
+    ctx = await playwright.request.newContext({
+        baseUrl: 'http://localhost:3000',
+    });
+});
+
+test.afterAll(async () => {
+    await ctx.dispose();
+})
+
+test('Create user', async () => {
+    const user = {
+        nameFirst: 'Sally',
+        nameLast: 'Wilson',
+        email: 'sally.wilson@email.com',
+        password: 'x'
+    }
+
+    const re = await ctx.post('user', { data: user });
+    vars.id = await re.json();
+
+    console.log('id', vars.id);
+
+    await expect(vars.id).toEqual(expect.any(Number));
+});
+
+test('Get users', async () => {
+    const re = await ctx.get('user');
+    vars.users = await re.json();
+
+    console.log(vars.users);
+
+    await expect(vars.users).toBeTruthy()
+});
