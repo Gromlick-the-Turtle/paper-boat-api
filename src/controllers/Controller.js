571bb49d3dc8c36f365180c44192bcb1ec5ffab2
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-10-29 16:07:01 -0400

    finish simplifying controller

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,13 +1,13 @@
 export default class Controller {
-    static async get (model, req, res) {
-        if (Object.hasOwn(model, 'noGet')) {
+    static async get (req, res) {
+        if (Object.hasOwn(this.model, 'noGet')) {
             throw Error(`${model} has no get function`);
         }
 
         res.json(await this.model.get(req.query, true));
     }
 
-    static async getOne (model, req, res) {
+    static async getOne (req, res) {
         if (Object.hasOwn(this.model, 'noGet')) {
             throw Error(`${this.model} has no get function`);
         }
@@ -15,7 +15,7 @@ export default class Controller {
         res.json((await this.model.get({ id: req.params.id }, true))[0]);
     }
 
-    static async create (model, req, res) {
+    static async create (req, res) {
         if (Object.hasOwn(this.model, 'noCreate')) {
             throw Error(`${this.model} has no create function`);
         }
@@ -23,7 +23,7 @@ export default class Controller {
         res.json((await this.model.create(req.body))[0].id);
     }
 
-    static async update (model, req, res) {
+    static async update (req, res) {
         if (Object.hasOwn(this.model, 'noUpdate')) {
             throw Error(`${this.model} has no update function`);
         }
@@ -33,7 +33,7 @@ export default class Controller {
         res.json();
     }
 
-    static async delete (model, req, res) {
+    static async delete (req, res) {
         if (Object.hasOwn(this.model, 'noUpdate')) {
             throw Error(`${this.model} has no update function`);
         }
@@ -43,10 +43,10 @@ export default class Controller {
     }
 
     static init () {
-        this.get    = this.get.bind(this, this.model);
-        this.getOne = this.getOne.bind(this, this.model);
-        this.create = this.create.bind(this, this.model);
-        this.update = this.update.bind(this, this.model);
-        this.delete = this.delete.bind(this, this.model);
+        this.get    = this.get.bind(this);
+        this.getOne = this.getOne.bind(this);
+        this.create = this.create.bind(this);
+        this.update = this.update.bind(this);
+        this.delete = this.delete.bind(this);
     }
 }
