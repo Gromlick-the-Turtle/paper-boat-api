43173a384356d7415cdee1c9a51f1c5f7fe6a103
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-10-31 12:19:06 -0400

    test sqlite db for cli/test state

---
+++ src/cli/sqlite-config.js
@@ -0,0 +1,31 @@
+import knex from 'knex';
+import fs from 'node:fs';
+import _ from 'lodash';
+
+const filename = './src/cli/db.sqlite';
+if (!fs.existsSync(filename)) {
+    fs.writeFileSync(filename, '');
+}
+
+const db = knex({
+    client: 'better-sqlite3',
+    connection: { filename },
+
+    wrapIdentifier: (val, origImp, ctx) => origImp(_.snakeCase(val)),
+
+    postProcessResponse: (result, ctx) => {
+        if (_.isArray(result)) {
+            return _.map(
+                result,
+                row => _.mapKeys(
+                    row,
+                    (val,key) => _.camelCase(key)
+                )
+            );
+        } else {
+            return _.mapKeys((val,key) => _.camelCase(key));
+        }
+    }
+});
+
+export default db;
