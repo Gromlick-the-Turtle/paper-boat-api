e92586cbc545e2f322a315192f29c3544e079b30
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-11-11 17:02:40 -0500

    update pb-fake to add fields

--- src/models/Address.js
+++ src/models/Address.js
@@ -3,8 +3,17 @@ import _ from 'lodash';
 import db from '#config/db';
 import Model from '#models/Model';
 
-export default class Address extends Model {
+export default class address extends Model {
     static table = 't_address';
+
+    static fields = {
+        id: Number,
+        line1: String,
+        line2: String,
+        stateId: Number,
+        countryId: Number,
+        zip: String
+    }
 
     static { this.init(); }
 }
