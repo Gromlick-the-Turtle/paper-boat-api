7868a83f7cce015913ab86d498c12b79cd49dca0
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-11-11 17:10:30 -0500

    new institution model

--- src/models/Institution.js
+++ src/models/Institution.js
@@ -2,13 +2,15 @@ import _ from 'lodash';
 
 import db from '#config/db';
 import Model from '#models/Model';
-import Address from '#models/Address';
 
 export default class Institution extends Model {
     static table = 't_institution';
 
-    static joins = {
-        address: this.hasOne(Address)
+    static fields = {
+        id: Number,
+        name: String,
+        description: String,
+        addressId: Number
     }
 
     static { this.init(); }
