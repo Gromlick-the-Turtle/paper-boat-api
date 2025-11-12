76d84324268be838311c9530be1f494e540e8b3d
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-11-12 10:03:51 -0500

    fake users

--- src/cli/fake.js
+++ src/cli/fake.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import bcrypt from 'bcryptjs';
 import { faker } from '@faker-js/faker';
 
 import db from '#config/pg-config';
+
 import Address from '#models/Address';
 import Institution from '#models/Institution';
 import User from '#models/User';
+import UserOrganization from '#models/UserOrganization';
 
 const randomId = async model => {
     const { min, max } = (
@@ -25,13 +28,13 @@ const randomId = async model => {
 };
 
 export default {
-    address: async addr => {
-        const address = {
+    async address (address) {
+        address = {
             line1: faker.location.streetAddress(),
             stateId: faker.number.int({ min: 1, max: 50 }),
             countryId: 1,
             zip: faker.number.int({ min: 12345, max: 99999 }),
-            ...addr
+            ...address
         };
 
         await Address.create(address);
@@ -39,16 +42,71 @@ export default {
         return address;
     },
 
-    institution: async inst => {
-        const institution = {
+    async institution (institution) {
+        institution = {
             name: faker.company.name(),
             description: faker.lorem.paragraph(5),
             addressId: await randomId(Address),
-            ...inst
+            ...institution
         };
 
         await Institution.create(institution);
 
         return institution;
-    }
+    },
+
+    async user (user, userOrganization) {
+        user = {
+            nameFirst: faker.person.firstName(),
+            nameLast: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: await bcrypt.hash('password', 10),
+            institutionId: await randomId(Institution),
+            ...user,
+        };
+
+        const userId = (await User.create(user))[0].id;
+
+        const role = faker.number.int({ min: 1, max: 3 });
+
+        userOrganization = {
+            userId,
+            organizationId: 1,
+            isAdmin: role==1,
+            isReviewer: role==2,
+            isAuthor: role==3,
+            ...userOrganization,
+        };
+
+        await UserOrganization.create(userOrganization);
+
+        return { userOrganization, ...user };
+    },
+
+    async author (user, userOrganization) {
+        return this.user(user, {
+            isAdmin: false,
+            isReviewer: false,
+            isAuthor: true,
+            ...userOrganization,
+        });
+    },
+
+    async reviewer (user, userOrganization) {
+        return this.user(user, {
+            isAdmin: false,
+            isReviewer: true,
+            isAuthor: false,
+            ...userOrganization,
+        });
+    },
+
+    async admin (user, userOrganization) {
+        return this.user(user, {
+            isAdmin: false,
+            isReviewer: false,
+            isAuthor: true,
+            ...userOrganization,
+        });
+    },
 }
