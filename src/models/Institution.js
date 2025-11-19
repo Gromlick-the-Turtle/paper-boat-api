a0be022aae06c5d92bcfd4d00e93f89039836b42
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-11-19 14:11:40 -0500

    uniq institution creation stuff

--- src/models/Institution.js
+++ src/models/Institution.js
@@ -6,11 +6,42 @@ import Model from '#models/Model';
 export default class Institution extends Model {
     static table = 't_institution';
 
+    static hidden = [
+        'snakeName',
+    ];
+
     static fields = {
         id: Number,
         name: String,
+        snakeName: String,
         description: String,
         addressId: Number
+    }
+
+    static create (item) {
+        item.snakeName = _.snakeCase(item.name);
+
+        return super.create(item)
+            .onConflict('snake_name')
+            .ignore();
+    }
+
+    static async upsertByNameMap (name) {
+        let id = (
+            db(this.table)
+            .select('id')
+            .where({ snakeName: _.snakeCase(name) })
+        )?.[0]?.id;
+
+        let id = (await qry)?.[0]?.id;
+
+        if (!_.isNil(id)) {
+            return id;
+        }
+
+        return (
+            await this.create({ name })
+        )?.[0]?.id;
     }
 
     static { this.init(); }
