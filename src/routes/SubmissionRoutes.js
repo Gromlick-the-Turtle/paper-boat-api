aae2bac47038f1b60e0f2327bdfb95c983fab4d6
Kyle Hewett <kyle.a.hewett@gmail.com>
2025-12-17 15:07:16 -0500

    stop dots

--- src/routes/SubmissionRoutes.js
+++ src/routes/SubmissionRoutes.js
@@ -1,9 +1,9 @@
 import SubmissionController from '#controllers/SubmissionController';
 
 export default function SubmissionRoutes (router) {
-    router.get('/submission',          SubmissionController..bind('get'));
-    router.get('/submission/:id',      SubmissionController..bind('getOne'));
-    router.post('/submission',         SubmissionController..bind('create'));
-    router.post('/submission/:id',     SubmissionController..bind('update'));
-    router.delete('/submission/:id',   SubmissionController..bind('delete'));
+    router.get('/submission',          SubmissionController.bind('get'));
+    router.get('/submission/:id',      SubmissionController.bind('getOne'));
+    router.post('/submission',         SubmissionController.bind('create'));
+    router.post('/submission/:id',     SubmissionController.bind('update'));
+    router.delete('/submission/:id',   SubmissionController.bind('delete'));
 }
