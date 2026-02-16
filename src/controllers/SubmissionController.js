61d599a73f7badb04d10d84680654eb5e8dad839
Kyle Hewett <kyle.a.hewett@gmail.com>
2026-02-16 16:16:49 -0500

    enforce ownership

--- src/controllers/SubmissionController.js
+++ src/controllers/SubmissionController.js
@@ -2,7 +2,22 @@ import _ from 'lodash';
 import Controller from '#controllers/Controller';
 import Submission from '#models/Submission';
 
+import ForbiddenError from '#errors/ForbiddenError';
+
 export default class SubmissionController extends Controller {
     withOrganization = false;
-    model = Submission; 
+    model = Submission;
+
+    async update (req, res) {
+        const re = await Submission.get({ id: req.params.id });
+        const userId = re?.[0]?.authorUserId;
+
+        if (userId != req.authedUser.userId) {
+            throw new ForbiddenError('You cannot change abstracts without permission');
+        }
+
+        await Submission.create(req.body);
+
+        res.json(req.params.id);
+    }
 }
