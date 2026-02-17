f3deb2298881e2182bf1e402a6c361d9d0be622c
Kyle Hewett <kyle.a.hewett@gmail.com>
2026-02-17 11:56:12 -0500

    add auth logic

--- src/controllers/SubmissionController.js
+++ src/controllers/SubmissionController.js
@@ -5,20 +5,44 @@ import Submission from '#models/Submission';
 import ForbiddenError from '#errors/ForbiddenError';
 
 export default class SubmissionController extends Controller {
-    withOrganization = false;
     model = Submission;
 
-    async update (req, res) {
-        const re = await Submission.get({ id: req.params.id });
-        const userId = re?.[0]?.authorUserId;
-        const id = re?.[0]?.id;
+    async checkAuth(params, user) {
+        const re = await Submission.get({
+            id: params.id,
+            organizationId: user.organizationId,
+        }, true);
 
-        if (userId != req.authedUser.userId) {
+        return re?.[0]?.authorUserId ?? false;
+    }
+
+    async create (req, res) {
+        if (!(await this.checkAuth(req.body, req.authedUser))) {
+            throw new ForbiddenError('You must submit to a permitted event');
+        }
+
+        res.json((await Submission.create(req.body))[0].id);
+    }
+
+    async update (req, res) {
+        const authId = await this.checkAuth(req.params, req.authedUser);
+
+        if (authId != req.authedUser.userId) {
             throw new ForbiddenError('You cannot change abstracts without permission');
         }
 
-        await Submission.update(req.body, { id });
+        await Submission.update(req.body, { id: req.params.id });
 
         res.json(req.params.id);
+    }
+
+    async delete (req, res) {
+        if (!(await this.checkAuth(req.params, req.authedUser))) {
+            throw new ForbiddenError('You are not permitted to delete that');
+        }
+
+        await Submission.delete({ id: req.params.id });
+
+        res.json();
     }
 }
